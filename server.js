3e3eeea8f9250ac7add39063c0d9addf95fdf2a2
masterkn <masterkn48@gmail.com>
2019-06-12 15:44:17 +0530

    i

--- server.js
+++ server.js
@@ -1,32 +1,28 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-const path =require('path');
-const items =require('./route/api/item');
-const app=express();
+const express = require('express');
+const mongoose = require('mongoose');
 
-//BodyParser Middleware
-app.use(bodyParser.json());
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+
+const app = express();
 
 // DB Config
-const db= require('./config/keys').mongoURI;
+const db = require('./config/keys').mongoURI;
+
+// Connect to MongoDB
 mongoose
   .connect(db)
-  .then(() => console.log("💻 Mondodb Connected"))
-  .catch(err => console.error(err));
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
 
-// route ./api/items
-app.use('/api/items', items);
+app.get('/', (req, res) => res.send('Hello World'));
 
-// Serve static assets if in production
-if(process.env.NODE_ENV === 'production'){
-  // set static folder
-  app.use(express.static('client/build'));
-  app.get('*', (req,res)=>{
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
+// Use Routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => `Server running on port ${port} 🔥`);
+app.listen(port, () => console.log(`Server running on port ${port}`));
