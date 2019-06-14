fbb816bd6791d62423400d6f56d87f6fd78c9cdb
masterkn <masterkn48@gmail.com>
2019-06-14 22:01:29 +0530

    frontend init

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+import Navbar from './component/layout/Navbar';
 import './App.css';
-
+import Footer from './component/layout/Footer';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <h1>My React App</h1>
+      <Footer />
     </div>
   );
 }
