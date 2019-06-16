7671ee5b62f1feb9a32be00c7e23d91188cd161f
masterkn <masterkn48@gmail.com>
2019-06-16 12:36:59 +0530

    login-logout

--- client/src/component/auth/Register.js
+++ client/src/component/auth/Register.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react"; 
 import PropTypes from 'prop-types';
 import {withRouter} from 'react-router-dom'; // use to pass router history to authAction
-import classnames from 'classnames';
 import {connect} from 'react-redux';
 import {registerUser} from '../../actions/authAction';
+import TextFieldGroup from '../common/TextFieldGroup.js';
 class Register extends Component {
     constructor(){
         super();
@@ -56,23 +56,38 @@ class Register extends Component {
                 <h1 className="display-4 text-center">Sign Up</h1>
                 <p className="lead text-center">Create your DevHub account</p>
                 <form onSubmit={this.onSubmit}>
-                    <div className="form-group">
-                    <input type="text" value={this.state.name} onChange={this.onChange} className={classnames("form-control form-control-lg",{'is-invalid':errors.name })} placeholder="Name" name="name" />
-                    {errors.name && (<div className="invalid-feedback">{errors.name}</div>)}
-                    </div>
-                    <div className="form-group">
-                    <input type="email" value={this.state.email} onChange={this.onChange} className={classnames("form-control form-control-lg",{'is-invalid':errors.email })} placeholder="Email Address" name="email" />
-                    <small className="form-text text-muted">This site uses Gravatar so if you want a profile image, use a Gravatar email</small>
-                    {errors.email && (<div className="invalid-feedback">{errors.email}</div>)}
-                    </div>
-                    <div className="form-group">
-                    <input type="password" value={this.state.password} onChange={this.onChange} className={classnames("form-control form-control-lg",{'is-invalid':errors.password })} placeholder="Password" name="password" />
-                    {errors.password && (<div className="invalid-feedback">{errors.password}</div>)}
-                    </div>
-                    <div className="form-group">
-                    <input type="password" value={this.state.password2}  onChange={this.onChange} className={classnames( "form-control form-control-lg",{'is-invalid':errors.password2 })} placeholder="Confirm Password" name="password2" />
-                    {errors.password2 && (<div className="invalid-feedback">{errors.password2}</div>)}
-                    </div>
+                    <TextFieldGroup
+                    placeholder="Name"
+                    name="name"
+                    value={this.state.name}
+                    onChange={this.onChange}
+                    errors={errors.name}
+                    />
+                    <TextFieldGroup
+                    placeholder="Email Address"
+                    name="email"
+                    type="email"
+                    info="This site uses Gravatar so if uoy want profile image use gravatar email"
+                    value={this.state.email}
+                    onChange={this.onChange}
+                    errors={errors.email}
+                    />
+                    <TextFieldGroup
+                    placeholder="Password"
+                    name="password"
+                    type="password"
+                    value={this.state.password}
+                    onChange={this.onChange}
+                    errors={errors.password}
+                    />
+                    <TextFieldGroup
+                    placeholder="Confirm Password"
+                    name="password2"
+                    type="password"
+                    value={this.state.password2}
+                    onChange={this.onChange}
+                    errors={errors.password2}
+                    />
                     <input type="submit" className="btn btn-info btn-block mt-4" />
                 </form>
                 </div>
