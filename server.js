6820895495694ab66d600950566df544ac95a616
masterkn <masterkn48@gmail.com>
2019-06-18 09:40:26 +0530

    prepare for delpoy

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const posts = require('./routes/api/posts');
 const bodyParser = require("body-parser");
 const app = express();
 const passport=require('passport');
-
+const path =require('path');
 // bodyparser
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
@@ -35,7 +35,13 @@ require("./config/passport")(passport);
 app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
-
+// serve static asstes if in production
+if(process.env.NODE_ENV==='production'){
+  app.use(express.static('client/build'));
+  app.get('*',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+  })
+}
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
