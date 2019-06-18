a7ada2c819e35a352589e09afb4dbb577e3076ba
masterkn <masterkn48@gmail.com>
2019-06-18 18:10:14 +0530

    finish 1

--- client/src/component/create-profile/CreateProfile.js
+++ client/src/component/create-profile/CreateProfile.js
@@ -221,7 +221,7 @@ class CreateProfile extends Component {
                 <input
                   type="submit"
                   value="Submit"
-                  className="btn btn-info btn-block mt-4"
+                  className="btn peach-gradient waves-effect btn-block mt-4"
                 />
               </form>
             </div>
